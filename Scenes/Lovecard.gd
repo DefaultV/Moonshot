e62066b8e4e0df1c2d7d7d1d73cd86170ff3b248
Defaultv <dozermailjd@gmail.com>
2020-11-27 23:54:45 +0100

    Misc updates and environmental additions

--- Scenes/Lovecard.gd
+++ Scenes/Lovecard.gd
@@ -6,17 +6,28 @@ extends Area2D
 # var b = "text"
 
 var ply;
+var pickup_ui;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered", self, "_on_body_enter");
 	ply = get_node("/root/World/Player");
+	pickup_ui = get_node("/root/World/UI/Itempickup");
 
 func _on_body_enter(body):
 	if (body.name == "Player"):
 		print("body")
 		hide();
 		ply.playtextblurp();
+		pickup_show();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+export var ui_text_pickup:String;
+func pickup_show():
+	pickup_ui.get_child(0).bbcode_text = "[center][color=black]\""+ ui_text_pickup +"\"[/color][/center]";
+	pickup_ui.texture = load(get_child(0).texture.resource_path);
+	pickup_ui.show();
+	yield(get_tree().create_timer(4.0),"timeout");
+	pickup_ui.hide();
--- Scripts/FetchItem.gd
+++ Scripts/FetchItem.gd
@@ -7,7 +7,9 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
+var pickup_ui;
 func _ready():
+	pickup_ui = get_node("/root/World/UI/Itempickup");
 	connect("body_entered", self, "_on_body_enter");
 
 
@@ -24,3 +26,13 @@ func _on_body_enter(body):
 		print("Gave player item: " + Item_to_give)
 		hide();
 		found = true;
+		pickup_show();
+		get_node("/root/World/Player").playtextblurp();
+
+export var ui_text_pickup:String;
+func pickup_show():
+	pickup_ui.get_child(0).bbcode_text = "[center][color=black]\""+ ui_text_pickup +"\"[/color][/center]";
+	pickup_ui.texture = load(get_child(0).texture.resource_path);
+	pickup_ui.show();
+	yield(get_tree().create_timer(4.0),"timeout");
+	pickup_ui.hide();
